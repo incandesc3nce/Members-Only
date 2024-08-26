44a00a06759ff1608e6873dbf3912f3f6a0f7158
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-26 19:55:39 +0700

    feat: create log out controller

---
+++ controllers/logoutController.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from 'express';
+import { nextTick } from 'process';
+
+const logoutController = async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	req.logout((err) => {
+		if (err) {
+			console.error(err);
+			return next(err);
+		}
+		res.redirect('/');
+	});
+	res.redirect('/');
+};
+
+export { logoutController };
