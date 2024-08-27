082b0c5e006a84a42d674caf3e66a41ba0c4de71
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-28 00:13:02 +0700

    feat: update the script with template users and messages

--- db/populateDB.js
+++ db/populateDB.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const Client = require('pg').Client;
+const hashPassword = require('../utils/hashPassword');
 
 const usersTable = `
   CREATE TABLE IF NOT EXISTS users (
@@ -23,7 +24,36 @@ const messagesTable = `
   );
 `;
 
-// TODO: insert users and some messages
+const users = [
+  {
+    full_name: 'John Doe',
+    username: 'john',
+    password: hashPassword('john123'),
+    is_member: false,
+    is_admin: false,
+  },
+  {
+    full_name: 'Jane Doe',
+    username: 'jane',
+    password: hashPassword('qwerty'),
+    is_member: false,
+    is_admin: false,
+  },
+];
+
+const messages = [
+  {
+    title: 'Hello, world!',
+    body: 'This thing we call "failure" is not the falling down, but the staying down.',
+    author_id: 1,
+  },
+  {
+    title: 'Ice T Quote',
+    body: "Don't hate the player; hate the game.",
+    author_id: 2,
+  },
+];
+
 
 const main = async () => {
 	const client = new Client({
@@ -37,9 +67,31 @@ const main = async () => {
 		await client.query(messagesTable);
 		console.log('Tables created successfully!');
 
-		// TODO: insert users and some messages
+		console.log('Populating users...');
+    for (const user of users) {
+      const { full_name, username, password, is_member, is_admin } = user;
+      const query = `
+        INSERT INTO users (full_name, username, password, is_member, is_admin)
+        VALUES ($1, $2, $3, $4, $5);
+      `;
+      await client.query(query, [full_name, username, password, is_member, is_admin]);
+    }
+    console.log('Users populated successfully!');
+
+    console.log('Populating messages...');
+
+    for (const message of messages) {
+      const { title, body, author_id } = message;
+      const query = `
+        INSERT INTO messages (title, body, author_id)
+        VALUES ($1, $2, $3);
+      `;
+      await client.query(query, [title, body, author_id]);
+    }
+    console.log('Messages populated successfully!');
 
 		await client.end();
+    console.log('Done.');
 	} catch (err) {
 		console.error(err);
 	}
