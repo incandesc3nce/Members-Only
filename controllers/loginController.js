a49d3b56c1bf5b04d5f4b2959ec384737bc36e2b
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-27 12:18:48 +0700

    feat: create login controller

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,1 +1,19 @@
 const { getUserByUsername } = require('../db/queries/usersQueries');
+const passport = require('../config/passport.config');
+
+const loginController = async (req, res) => {
+  res.render('login', {
+    title: 'Log In',
+  });
+}
+
+const loginPostController = passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true,
+});
+
+module.exports = {
+  loginController,
+  loginPostController
+};
