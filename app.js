b215e37ccf3de00ee6c0e598cab8ed52c9296746
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-27 10:32:16 +0700

    feat: configure passport

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
-
 const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const path = require('path');
+const bcrypt = require('bcryptjs');
+const usersQueries = require('./db/queries/usersQueries');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -9,6 +13,54 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(session({
+	secret: process.env.SESSION_SECRET || 'secret',
+	resave: false,
+	saveUninitialized: false,
+	cookie: { sameSite: 'strict' },
+}));
+
+app.use(passport.session());
+
+passport.use(new LocalStrategy(
+	async (username, password, done) => {
+		try {
+			const user = await usersQueries.getUserByUsername(username);
+			if (!user) {
+				return done(null, false, { message: 'Incorrect username or password.' });
+			}
+			
+			const valid = await bcrypt.compare(password, user.password);
+			if (!valid) {
+				return done(null, false, { message: 'Incorrect username or password.' });
+			}
+			
+			return done(null, user);
+		} catch (err) {
+			console.error('There was an error logging in:', err);
+			return done(err);
+		}
+	}
+));
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+	try {
+		const user = await usersQueries.getUserById(id);
+		done(null, user);
+	} catch(err) {
+		done(err);
+	}
+});
+
+
+app.get('/', (req, res) => {
+	res.render('index', { title: 'Home' });
+});
 
 app.use((req, res, next) => {
 	res.status(404).send('404 Not Found');
