0c19645677e5ec2e24714686b22abb4c6263b2c0
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-27 12:14:13 +0700

    feat: create logout controller

---
+++ controllers/logoutController.js
@@ -0,0 +1,10 @@
+const logoutController = (req, res, next) => {
+	req.logout((err) => {
+		if (err) {
+			return next(err);
+		}
+		res.redirect('/');
+	});
+};
+
+module.exports = logoutController;
